12ae22126043ff2a4ba19c1f9a174d0f1531647e
simon231442 <simon2314@hotmail.com>
2026-04-14 09:52:25 +0200

    lenovo : avance sur ex01

--- ex01/Serializer.cpp
+++ ex01/Serializer.cpp
@@ -1,1 +1,11 @@
 #include "serializer.hpp"
+
+uintptr_t	serializer::serialize(Data* ptr)
+{
+	return (reinterpret_cast<uintptr_t>(ptr));
+}
+
+Data* 		serializer::deserialize(uintptr_t raw)
+{
+	return (reinterpret_cast<Data*>(raw));
+}
